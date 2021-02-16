8a68b95e854e962d2173919c8b14e9319a777e1c
uri paz <uripaz9@gmail.com>
2021-02-16 20:43:26 +0200

    test summery description

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
